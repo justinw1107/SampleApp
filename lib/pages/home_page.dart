61828cf2f22b96f6acf693062fbb6236fc09046e
justinw1107 <justinwan1107@gmail.com>
2022-10-26 22:28:28 +0100

    created login

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:sampleapp/service/auth_service.dart';
+import 'package:sampleapp/widgets/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  AuthService authService = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                nextScreen(context, SearchPage());
+              },
+              icon: const Icon(
+                Icons.search,
+              ))
+        ],
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "Groups",
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 27),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+class Constants {
+  static String appId = "1:310326778349:web:0b64baa4b667ef473defe4";
+  static String apiKey = "AIzaSyC4N8Nzt2nun5WoxY6h6tNUBYo1b7SnTKk";
+  static String messagingSenderId = "310326778349";
+  static String projectId = "sampleapp-981f1";
+  final primaryColor = const Color(0xFFee7b64);
+}
